adce7e14e7a2d20fce2643b710732ff43d27bdd2
Jaakul <jackw1689@gmail.com>
2022-09-08 12:40:00 -0700

    created layout and css structure

---
+++ script.js
@@ -0,0 +1,19 @@
+(function (){
+
+playerCreator= function (name,marker){
+    return {
+        name,marker
+    }
+    
+}
+,
+
+array = [1,2,3,4]
+,
+
+currentPlayers = function currentPlayers(player1,player2){
+    return {player1, player2}
+}
+
+return{booty:currentPlayers}
+})();
